68ed96d83cced52136529095ce69616479a40067
Sathish2909 <sathishshanmugam2904@gmail.com>
2024-12-15 13:48:22 +0530

    user

--- api/controllers/authcontroller.js
+++ api/controllers/authcontroller.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js"
 import bcrypt from "bcrypt";
 import { createerror } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const register=async(req,res,next)=>{
     try{
@@ -33,8 +34,13 @@ export const login=async(req,res,next)=>{
         }
         const isPasswordCorrect=await bcrypt.compare(req.body.password,user.password)
         if(!isPasswordCorrect) return next(createerror(400,"Invalid password or username"))
+
+
+        const token=jwt.sign({id:user._id,isAdmin:user.isAdmin},process.env.JWT)
+
+        const {password,isAdmin,...otherDetails}=user._doc;
         
-        res.status(200).json(user)
+        res.status(200).json({...otherDetails});
     }
 
 
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,10 +1,19 @@
 import express from "express";
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/usercontroller.js";
 
 const router=express.Router();
 
-router.get("/",(req,res)=>{
-    res.send("this is auth");
-});
 
 
-export default router;
+router.put("/:id",updateUser);
+
+
+router.delete("/:id",deleteUser);
+
+
+
+router.get("/:id",getUser);
+
+
+router.get("/",getUsers); 
+export default router; 

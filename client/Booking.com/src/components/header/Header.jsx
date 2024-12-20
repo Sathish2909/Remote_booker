8f1d95a8d08b3ff40a0489d024fa9983b6bb2a61
Sathish2909 <sathishshanmugam2904@gmail.com>
2024-12-20 13:54:10 +0530

    date range used

--- client/Booking.com/src/components/header/Header.jsx
+++ client/Booking.com/src/components/header/Header.jsx
@@ -1,8 +1,19 @@
 import './Header.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBed,faCar,faTaxi,faPlane, faCalendarDays, faPerson} from "@fortawesome/free-solid-svg-icons";
+import {DateRange} from 'react-date-range';
+import React,{ useState } from 'react';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css';
 
 const Header = () => {
+  const [date,setDate]=useState([
+    {
+        startDate :new Date(),
+        endDate :null,
+        key:'selection'
+    }
+  ]);
   return (
     <div className="header">
         <div className="headerList">
@@ -35,15 +46,25 @@ const Header = () => {
             <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faBed} className='header-icon'/>
                 <input type="text" placeholder='where are you going'
-                className='headerSearchINput' />
+                className='headerSearchInput' />
             </div>
             <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faCalendarDays } className='header-icon'/>
                 <span className='headerSearchText'> date to date</span>
+                <DateRange
+                editableDateInputs={true}
+                onChange={item=>setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                className='date'
+                />
             </div>
             <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faPerson} className='header-icon'/>
                 <span className='headerSearchText'>2 adults 1 children</span>
+            </div>
+            <div className="headerSearchItem">
+                 <button className="headerBtn">Search</button>
                 
             </div>
         </div>

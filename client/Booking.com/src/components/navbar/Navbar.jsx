92bd7d0f4533cd002df251cb2328f3b7a05da48a
Sathish2909 <sathishshanmugam2904@gmail.com>
2025-06-22 07:19:52 +0530

    frontend done

--- client/Booking.com/src/components/navbar/Navbar.jsx
+++ client/Booking.com/src/components/navbar/Navbar.jsx
@@ -1,18 +1,17 @@
-import "./Navbar.css";
+import "./Navbar.css"
 
 const Navbar = () => {
   return (
-    <div className="Navbar">
-        <div className="navcontainer">
-            <span className="logo">Sandy's Booker</span>
-            <div className="navItems">
-                <button className="navButton">Register</button>
-                <button className="navButton">Login</button>
-
-            </div>
+    <div className="navbar">
+      <div className="navContainer">
+        <span className="logo">Book Voyage</span>
+        <div className="navItems">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
         </div>
+      </div>
     </div>
   )
 }
 
-export default Navbar;
+export default Navbar
